76eb65aac09bdb2cc59efb829ae8699ea0a23366
Kevin Smith <ksmith1216@gmail.com>
2016-10-22 18:06:19 +0000

    solution

--- create_box/main.py
+++ create_box/main.py
@@ -1,9 +1,31 @@
 """This is the entry point of the program."""
 
 
+
 def create_box(height, width, character):
-    pass
+    box= ''
+    if height < 1 or width < 1:
+        return 'Error'
+    for x in range(height):
+        box+= character*width+'\n'
+    return box
 
 
-if __name__ == '__main__':
-    create_box(3, 4, '*')
+'''
+def create_box(height, width, character):
+    full_row = character * width
+    edge_row = character + ' '*(width-2) + character
+    interior = ''
+    
+    if height < 1 or width < 1:
+        return 'Error'
+    for x in range(1, height-1):
+        interior += edge_row+'\n'
+    
+    box = (full_row+'\n') + (interior) + (full_row)
+    
+    return box
+    
+lebox=create_box(10, 3, '7')
+print(lebox)
+'''
